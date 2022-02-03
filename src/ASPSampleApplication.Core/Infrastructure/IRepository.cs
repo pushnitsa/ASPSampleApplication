ad85c93a1dd74695347e814750d1703488ea8be6
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-02-03 21:47:18 +0200

    Add repository

---
+++ src/ASPSampleApplication.Core/Infrastructure/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using ASPSampleApplication.Core.Infrastructure;
+
+namespace ASPSampleApplication.Data.Repositories
+{
+    public interface IRepository : IAsyncDisposable
+    {
+        IUnitOfWork UnitOfWork { get; }
+
+        void Add<T>(T entity);
+        void Update<T>(T entity);
+        void Delete<T>(T entity);
+    }
+}
---
+++ src/ASPSampleApplication.Data/Repositories/EntryRepository.cs
@@ -0,0 +1,44 @@
+﻿using ASPSampleApplication.Core.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPSampleApplication.Data.Repositories
+{
+    public class EntryRepository : IRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public EntryRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            UnitOfWork = new DbContextUnitOfWork(dbContext);
+        }
+
+        public IUnitOfWork UnitOfWork { get; }
+
+        public void Add<T>(T entity)
+        {
+            _dbContext.Add(entity);
+        }
+
+        public void Update<T>(T entity)
+        {
+            _dbContext.Update(entity);
+        }
+
+        public void Delete<T>(T entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual ValueTask Dispose(bool disposing)
+        {
+            return _dbContext.DisposeAsync();
+        }
+    }
+}
