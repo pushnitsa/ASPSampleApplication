25a66beaf7b53dda2de4267e62db677ca4c7b3d0
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-02-03 13:33:57 +0200

    Initial commit

---
+++ src/ASPSampleApplication.Web/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace ASPSampleApplication.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
